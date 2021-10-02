7eab8c514113de6d28cec66e3bcb1f7260b5bc78
SuzuSys <sigmatics@outlook.jp>
2021-10-02 16:30:22 +0000

    update backend

--- backend/index.js
+++ backend/index.js
@@ -8,3 +8,20 @@ const out = fs.createWriteStream("info.log");
 const logger = new console.Console(out);
 logger.log('---ERROR STORAGE---');
 // end{debug setting}
+
+const connectOption = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+mongoose.connect("mongodb://localhost/MemorizationApplication", connectOption);
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(process.env.PORT || 3000);
